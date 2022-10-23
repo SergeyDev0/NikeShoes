6a28239dd3e962ccec664da89ecd8829478116b9
makarkapar <makarkapar@gmail.com>
2022-10-23 15:48:13 +0300

    commit

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -1,8 +1,10 @@
 import logoIcon from '../../icons/logo.svg'
+import bantIcon from '../../icons/bant.svg'
 
 const Logo = () => {
     return(
         <div className='header__wrapper-logo'>
+            <img width={20} className='logo-bant' src={bantIcon} alt="logo-bant" />
             <img width={128} height={16.5} className='header-logo' src={logoIcon} alt="logo"/>
         </div>
 
